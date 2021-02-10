0a0a60a3fd0dcc338d9ea8f624211f2aa97131a0
Alexa-R <aleksa.rybakova00@mail.ru>
2021-02-10 22:32:53 +0300

    add RandomHelper

---
+++ FinalProject/Helpers/RandomHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace FinalProject.Helpers
+{
+    public static class RandomHelper
+    {
+        private static readonly Random Random = new Random();
+        private const string WithoutSpacesPattern = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public static string GetRandomString(int length) =>
+            new string(Enumerable.Repeat(WithoutSpacesPattern, length).Select(s => s[Random.Next(s.Length)]).ToArray());
+    }
+}
