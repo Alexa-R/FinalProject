d82a2b8fa25862863b9e9577b07bfa85a299a748
Alexa-R <aleksa.rybakova00@mail.ru>
2021-02-11 08:48:22 +0300

    move waiters from WaitHelper to WrapperWebElement

--- FinalProject/Helpers/WaitHelper.cs
+++ FinalProject/Helpers/WaitHelper.cs
@@ -10,39 +10,16 @@ namespace FinalProject.Helpers
         public static TimeSpan Timeout = TimeSpan.FromSeconds(15);
         public static TimeSpan PollingInterval = TimeSpan.FromMilliseconds(500);
 
-        public static WebDriverWait GetExplicitWait(TimeSpan timeout = default, TimeSpan pollingInterval = default)
+        public static WebDriverWait GetExplicitWait(TimeSpan timeout = default, TimeSpan pollingInterval = default, Type[] exceptionTypes = null)
         {
             var wait = new WebDriverWait(WebDriverFactory.Driver, timeout.Ticks == 0 ? Timeout : timeout)
             {
                 PollingInterval = pollingInterval.Ticks == 0 ? PollingInterval : pollingInterval
             };
 
+            wait.IgnoreExceptionTypes(exceptionTypes ?? new[]{ typeof(StaleElementReferenceException) });
+            
             return wait;
-        }
-
-        public static void WaitUntilElementIsDisplayed(IWebElement element, TimeSpan timeout = default, TimeSpan pollingInterval = default)
-        {
-            GetExplicitWait(timeout, pollingInterval).Until((d) => element.Displayed);
-        }
-        
-        public static void WaitUntilElementToBeClickable(IWebElement element, TimeSpan timeout = default, TimeSpan pollingInterval = default)
-        {
-            GetExplicitWait(timeout, pollingInterval).Until(ExpectedConditions.ElementToBeClickable(element));
-        }
-
-        public static void WaitUntilElementStaleness(IWebElement element, TimeSpan timeout = default, TimeSpan pollingInterval = default)
-        {
-            GetExplicitWait(timeout, pollingInterval).Until(ExpectedConditions.StalenessOf(element));
-        }
-
-        public static void WaitUntilElementExists(By @by, TimeSpan timeout = default, TimeSpan pollingInterval = default)
-        {
-            GetExplicitWait(timeout, pollingInterval).Until(ExpectedConditions.ElementExists(@by));
-        }
-
-        public static void WaitUntilInvisibilityOfElementWithText(By @by, string text, TimeSpan timeout = default, TimeSpan pollingInterval = default)
-        {
-            GetExplicitWait(timeout, pollingInterval).Until(ExpectedConditions.InvisibilityOfElementWithText(@by, text));
         }
     }
 }
--- FinalProject/WrapperElement/WrapperWebElement.cs
+++ FinalProject/WrapperElement/WrapperWebElement.cs
@@ -4,88 +4,183 @@ using System.Drawing;
 using FinalProject.Helpers;
 using FinalProject.WrapperFactory;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace FinalProject.WrapperElement
 {
     public class WrapperWebElement : IWebElement
     {
+        public static TimeSpan DefaultPollingInterval = TimeSpan.FromMilliseconds(500);
+        public static TimeSpan Timeout = TimeSpan.FromSeconds(15);
+        private readonly bool _canCache;
+        private readonly By _by;
         private IWebElement _webElementImplementation;
+
+        private IWebElement WebElementImplementation
+        {
+            get
+            {
+                IWebElement result;
+                if (_webElementImplementation == null)
+                {
+                    result = GetElementWhenExists(_by);
+
+                    if (_canCache)
+                    {
+                        _webElementImplementation = result;
+                    }
+                }
+                else
+                {
+                    result = _webElementImplementation;
+                }
+
+                return result;
+            }
+        }
 
         public WrapperWebElement(By @by)
         {
-            try
-            {
-                WaitHelper.WaitUntilElementExists(@by, TimeSpan.FromSeconds(10));
-            }
-            catch (WebDriverTimeoutException)
-            {
-            }
-            _webElementImplementation = WebDriverFactory.Driver.FindElement(@by);
+            _by = @by;
+            _canCache = true;
         }
 
         public IWebElement FindElement(By @by)
         {
-            return _webElementImplementation.FindElement(@by);
+            return WebElementImplementation.FindElement(@by);
         }
 
         public ReadOnlyCollection<IWebElement> FindElements(By @by)
         {
-            return _webElementImplementation.FindElements(@by);
+            return WebElementImplementation.FindElements(@by);
         }
 
-        public void Clear()
+        public void Clear() => WaitHelper.GetExplicitWait()
+            .Until(d =>
+            {
+                WebElementImplementation.Clear();
+
+                return true;
+            });
+
+        public void SendKeys(string text) => WaitHelper.GetExplicitWait()
+            .Until(d =>
+            {
+                WebElementImplementation.SendKeys(text);
+
+                return true;
+            });
+
+        public void Submit() => WaitHelper.GetExplicitWait(exceptionTypes: new[] { typeof(StaleElementReferenceException), typeof(ElementClickInterceptedException), typeof(ElementNotInteractableException) })
+            .Until(d =>
+            {
+                WebElementImplementation.Click();
+
+                return true;
+            });
+
+        public void Click() => WaitHelper.GetExplicitWait(exceptionTypes: new[] { typeof(StaleElementReferenceException), typeof(ElementClickInterceptedException), typeof(ElementNotInteractableException) })
+            .Until(d =>
+            {
+                WebElementImplementation.Click();
+
+                return true;
+            });
+
+        public string GetAttribute(string attributeName) => WaitHelper.GetExplicitWait().Until(d => WebElementImplementation.GetAttribute(attributeName));
+        
+        public string GetProperty(string propertyName) => WaitHelper.GetExplicitWait().Until(d => WebElementImplementation.GetProperty(propertyName));
+        
+        public string GetCssValue(string propertyName) => WaitHelper.GetExplicitWait()
+            .Until(d => WebElementImplementation.GetCssValue(propertyName));
+
+        public string TagName => WaitHelper.GetExplicitWait().Until(d => WebElementImplementation.TagName);
+
+        public string Text => WaitHelper.GetExplicitWait().Until(d => WebElementImplementation.Text);
+
+        public bool Enabled => WaitHelper.GetExplicitWait().Until(d => WebElementImplementation.Enabled);
+
+        public bool Selected => WaitHelper.GetExplicitWait().Until(d => WebElementImplementation.Selected);
+
+        public Point Location => WaitHelper.GetExplicitWait().Until(d => WebElementImplementation.Location);
+
+        public Size Size => WaitHelper.GetExplicitWait().Until(d => WebElementImplementation.Size);
+        
+        public bool Displayed
         {
-            WaitHelper.WaitUntilElementIsDisplayed(_webElementImplementation);
-            _webElementImplementation.Clear();
+            get
+            {
+                try
+                {
+                    WaitHelper.GetExplicitWait().Until(d => WebElementImplementation.Displayed);
+                    return WebElementImplementation.Displayed;
+                }
+                catch (WebDriverTimeoutException)
+                {
+                    return false;
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            }
         }
+        
+        public void WaitForElementIsPresent(int? timeout = null) =>
+            WaitForElementExistence(timeout == null ? Timeout : TimeSpan.FromMilliseconds((int) timeout));
 
-        public void SendKeys(string text)
+        public void WaitForElementDisappear(int? timeout = null) =>
+            WaitForElementExistence(timeout == null ? Timeout : TimeSpan.FromMilliseconds((int) timeout), DefaultPollingInterval, false);
+
+        public void WaitForElementIsEnabled(int? timeout = null) =>
+            WaitHelper.GetExplicitWait(timeout == null ? Timeout : TimeSpan.FromMilliseconds((int)timeout))
+                .Until(d => WebElementImplementation.Enabled);
+
+        public void WaitForElementIsDisabled(int? timeout = null) =>
+            WaitHelper.GetExplicitWait(timeout == null ? Timeout : TimeSpan.FromMilliseconds((int)timeout))
+                .Until(d => !WebElementImplementation.Enabled);
+
+        public void WaitForElementIsDisplayed(int? timeout = null) =>
+            WaitHelper.GetExplicitWait(timeout == null ? Timeout : TimeSpan.FromMilliseconds((int)timeout))
+                .Until(d => WebElementImplementation.Displayed);
+
+        public void WaitForElementToBeClickable(int? timeout = null) =>
+            WaitHelper.GetExplicitWait(timeout == null ? Timeout : TimeSpan.FromMilliseconds((int)timeout))
+                .Until(ExpectedConditions.ElementToBeClickable(WebElementImplementation));
+
+        public void WaitForElementIsStale(int? timeout = null) =>
+            WaitHelper.GetExplicitWait(timeout == null ? Timeout : TimeSpan.FromMilliseconds((int)timeout))
+                .Until(ExpectedConditions.StalenessOf(WebElementImplementation));
+
+        public void WaitForInvisibilityOfElementWithText(string text, int? timeout = null) =>
+            WaitHelper.GetExplicitWait(timeout == null ? Timeout : TimeSpan.FromMilliseconds((int)timeout))
+                .Until(ExpectedConditions.InvisibilityOfElementWithText(_by, text));
+
+        private IWebElement GetElementWhenExists(By @by, int timeout = default)
         {
-            WaitHelper.WaitUntilElementToBeClickable(_webElementImplementation);
-            _webElementImplementation.SendKeys(text);
+            IsExists(timeout, true);
+            return _webElementImplementation = WebDriverFactory.Driver.FindElement(@by);
         }
 
-        public void Submit()
+        private bool IsExists(int timeout, bool shouldExists) => IsExists(TimeSpan.FromSeconds(timeout), shouldExists);
+
+        private bool IsExists(TimeSpan timeout, bool shouldExists)
         {
-            WaitHelper.WaitUntilElementToBeClickable(_webElementImplementation);
-            _webElementImplementation.Submit();
+            try
+            {
+                WaitForElementExistence(timeout, shouldExists: shouldExists);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
-        public void Click()
-        {
-            WaitHelper.WaitUntilElementToBeClickable(_webElementImplementation);
-            _webElementImplementation.Click();
-        }
-
-        public string GetAttribute(string attributeName)
-        {
-            WaitHelper.WaitUntilElementIsDisplayed(_webElementImplementation);
-            return _webElementImplementation.GetAttribute(attributeName);
-        }
-
-        public string GetProperty(string propertyName)
-        {
-            WaitHelper.WaitUntilElementIsDisplayed(_webElementImplementation);
-            return _webElementImplementation.GetProperty(propertyName);
-        }
-
-        public string GetCssValue(string propertyName)
-        {
-            return _webElementImplementation.GetCssValue(propertyName);
-        }
-
-        public string TagName => _webElementImplementation.TagName;
-
-        public string Text => _webElementImplementation.Text;
-
-        public bool Enabled => _webElementImplementation.Enabled;
-
-        public bool Selected => _webElementImplementation.Selected;
-
-        public Point Location => _webElementImplementation.Location;
-
-        public Size Size => _webElementImplementation.Size;
-
-        public bool Displayed => _webElementImplementation.Displayed;
+        private void WaitForElementExistence(TimeSpan? timeout = null, TimeSpan? pollingInterval = null,
+            bool shouldExists = true) =>
+            WaitHelper.GetExplicitWait(timeout ?? Timeout, pollingInterval ?? DefaultPollingInterval)
+                .Until(d => d.FindElements(_by).Count != 0 == shouldExists);
     }
 }
