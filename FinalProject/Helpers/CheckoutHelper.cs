260c012a483c5f4c0d831d88cf77b680840c4ec8
Alexa-R <aleksa.rybakova00@mail.ru>
2021-02-11 08:40:18 +0300

    add AddNewBillingAddress

--- FinalProject/Helpers/CheckoutHelper.cs
+++ FinalProject/Helpers/CheckoutHelper.cs
@@ -4,19 +4,47 @@ namespace FinalProject.Helpers
 {
     public static class CheckoutHelper
     {
-        public static void AddNewAddress()
-        {
+        private static string companyName = "TestCompany";
+        private static string phoneNumber = "1234567";
+        private static string firstAddress = "77 Massachusetts Avenue";
+        private static string countryValue = "US";
+        private static string stateValue = "MA";
+        private static string county = "Middlesex";
+        private static string city = "Cambridge";
+        private static string zipCode = "02139";
+
+        public static void AddNewShippingAddress()
+        { 
+            var nickname = RandomHelper.GetRandomString(12); 
             Pages.CheckoutPage.ClickAddNewAddressButton();
-            Pages.CheckoutPage.EnterNickname("JaneSmith");
-            Pages.CheckoutPage.EnterCompanyName("Company");
-            Pages.CheckoutPage.EnterPhoneNumber("1234567");
-            Pages.CheckoutPage.EnterFirstAddress("77 Massachusetts Avenue");
-            Pages.CheckoutPage.SelectCountryFromCountriesDropdownMenu("US");
-            Pages.CheckoutPage.SelectStateFromStatesDropdownMenu("MA");
-            Pages.CheckoutPage.EnterCounty("Middlesex");
-            Pages.CheckoutPage.EnterCity("Cambridge");
-            Pages.CheckoutPage.EnterZipCode("02139");
+            Pages.CheckoutPage.EnterNickname(nickname);
+            Pages.CheckoutPage.EnterCompanyName(companyName);
+            Pages.CheckoutPage.EnterPhoneNumber(phoneNumber);
+            Pages.CheckoutPage.EnterFirstAddress(firstAddress);
+            Pages.CheckoutPage.SelectCountryFromCountriesDropdownMenu(countryValue);
+            Pages.CheckoutPage.SelectStateFromStatesDropdownMenu(stateValue);
+            Pages.CheckoutPage.EnterCounty(county);
+            Pages.CheckoutPage.EnterCity(city);
+            Pages.CheckoutPage.EnterZipCode(zipCode);
             Pages.CheckoutPage.ClickSaveButton();
+            Pages.CheckoutPage.ClickAddressRadioButton(nickname);
+        }
+
+        public static void AddNewBillingAddress()
+        {
+            var nickname = RandomHelper.GetRandomString(12);
+            Pages.CheckoutPage.ClickAddNewAddressButton();
+            Pages.CheckoutPage.EnterNickname(nickname);
+            Pages.CheckoutPage.EnterCompanyName(companyName);
+            Pages.CheckoutPage.EnterPhoneNumber(phoneNumber);
+            Pages.CheckoutPage.EnterFirstAddress(firstAddress);
+            Pages.CheckoutPage.SelectCountryFromCountriesDropdownMenu(countryValue);
+            Pages.CheckoutPage.SelectStateFromStatesDropdownMenu(stateValue);
+            Pages.CheckoutPage.EnterCounty(county);
+            Pages.CheckoutPage.EnterCity(city);
+            Pages.CheckoutPage.EnterZipCode(zipCode);
+            Pages.CheckoutPage.ClickSaveButton();
+            Pages.CheckoutPage.ClickAddressRadioButton(nickname);
         }
     }
 }
