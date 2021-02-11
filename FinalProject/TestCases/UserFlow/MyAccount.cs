5c9def11cc1f511c483811b0a23844c4f302d4f4
Alexa-R <aleksa.rybakova00@mail.ru>
2021-02-11 10:27:41 +0300

    fixes for MyAccount

--- FinalProject/TestCases/UserFlow/MyAccount.cs
+++ FinalProject/TestCases/UserFlow/MyAccount.cs
@@ -20,13 +20,10 @@ namespace FinalProject.TestCases.UserFlow
             Pages.BasePage.ClickBagButton();
             Pages.BasePage.ClickViewBagButton();
             Pages.CartPage.ClickContinueToCheckout();
-            
             Pages.CheckoutPage.ClickChangeShippingAddressLink();
             CheckoutHelper.AddNewShippingAddress();
-            
             Pages.CheckoutPage.ClickChangeBillingAddressLink();
             CheckoutHelper.AddNewBillingAddress();
-            
             Pages.CheckoutPage.ClickPaymentMethodDropdownMenu();
             Pages.CheckoutPage.SelectPaymentMethodFromPaymentMethodDropdownList(PaymentMethodNamesConstants.VisaEndingIn1026);
             Pages.CheckoutPage.ClickPlaceOrderButton();
