fb7e8d498859413c6fce22303f12178a8b6f1d8e
Alexa-R <aleksa.rybakova00@mail.ru>
2021-02-15 10:00:51 +0300

    add ExtentReports

---
+++ FinalProject/TestCases/BaseSuite.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FinalProject.Helpers;
+using NUnit.Framework;
+
+namespace FinalProject.TestCases
+{
+    [TestFixture]
+    public class BaseSuite
+    {
+        protected ExtentReportsHelper Extent;
+
+        [SetUp]
+        public void SetUpReporter()
+        {
+            Extent = ExtentReportsHelper.GetExtentReportsHelper();
+        }
+
+        [TearDown]
+        public void CloseReporter()
+        {
+            try
+            {
+                Extent.Close();
+            }
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+        }
+    }
+}
--- FinalProject/TestCases/BaseTest.cs
+++ FinalProject/TestCases/BaseTest.cs
@@ -1,17 +1,19 @@
+using System;
 using System.Configuration;
 using FinalProject.Constants;
 using FinalProject.PageObjects;
 using FinalProject.WrapperFactory;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 
 namespace FinalProject.TestCases
 {
-    [TestFixture]
-    public class BaseTest
+    public class BaseTest : BaseSuite
     {
         [SetUp]
         public void SetUpTest()
         {
+            Extent.CreateTest(TestContext.CurrentContext.Test.Name);
             WebDriverFactory.InitBrowser("Chrome");
             WebDriverFactory.GoToUrl(ConfigurationManager.AppSettings["URL"]);
             WebDriverFactory.Driver.Manage().Window.Maximize();
@@ -21,7 +23,33 @@ namespace FinalProject.TestCases
         [TearDown]
         public void TearDownTest()
         {
-            WebDriverFactory.CloseAllDrivers();
+            try
+            {
+                var status = TestContext.CurrentContext.Result.Outcome.Status;
+                var stacktrace = TestContext.CurrentContext.Result.StackTrace;
+                var errorMessage = "<pre>" + TestContext.CurrentContext.Result.Message + "</pre>";
+                
+                switch (status)
+                {
+                    case TestStatus.Failed:
+                        Extent.SetTestStatusFail($"<br>{errorMessage}<br>Stack Trace: <br>{stacktrace}<br>");
+                        break;
+                    case TestStatus.Skipped:
+                        Extent.SetTestStatusSkipped();
+                        break;
+                    default:
+                        Extent.SetTestStatusPass();
+                        break;
+                }
+            }
+            catch (Exception exc)
+            {
+                throw (exc);
+            }
+            finally
+            {
+                WebDriverFactory.CloseAllDrivers();
+            }
         }
 
         private void LoginAsUser()
