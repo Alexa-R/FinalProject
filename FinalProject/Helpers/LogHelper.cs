150c8948970ebf7dac538bc7af04a4491d95dfcb
Aliaksandra Rybakova <Aliaksandra.Rybakova@itechart-group.com>
2021-03-12 11:41:47 +0300

    add SetStepStatusPass to LogHelper.Info

--- FinalProject/Helpers/LogHelper.cs
+++ FinalProject/Helpers/LogHelper.cs
@@ -4,11 +4,12 @@ namespace FinalProject.Helpers
 {
     public static class LogHelper
     {
-        private static readonly ILog Logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         public static void Info(string message)
         {
             Logger.Info(message);
+            ExtentReportsHelper.GetExtentReportsHelper().SetStepStatusPass(message);
         }
     }
 }
